76af8c841b3b524a03777111d8ebb32d259145f9
lght-hcker <94937754+lght-hcker@users.noreply.github.com>
2023-06-16 13:24:33 +0000

    exam

---
+++ jquery.js
@@ -0,0 +1,114 @@
+$('body').css({
+    'margin': '0',
+    'font-family': 'Arial, sans-serif',
+    'background-color': 'rgb(255, 255, 255)'
+});
+
+$('nav').css({
+    'display': 'flex',
+    'justify-content': 'space-evenly',
+    'align-items': 'center',
+    'background-color': 'dimgray',
+    'padding': '10px',
+    'text-decoration': 'none'
+});
+
+$('.salem').css({
+    'color': '#da491c',
+    'font-size': '30px',
+    'text-decoration': 'none'
+});
+
+$('.menu').css({
+    'list-style-type': 'none',
+    'margin': '10px',
+    'padding': '0',
+    'display': 'flex'
+});
+
+$('.menu li').css({
+    'margin-left': '40px',
+    'justify-content': 'center'
+});
+
+$('.menu li a').css({
+    'color': '#fff',
+    'text-decoration': 'none'
+});
+
+$('.menu li a:hover').css({
+    'color': '#da491c',
+    'transition': '0.5s'
+});
+
+$('nav .work a').css({
+    'color': '#da491c'
+});
+
+$('.love').css({
+    'color': '#da491c'
+});
+
+$('.one').css({
+    'text-align': 'center',
+    'padding': '40px',
+    'color': 'white'
+});
+
+$('.ligne').css({
+    'border': '1px solid #da491c',
+    'position': 'relative',
+    'top': '20px',
+    'margin-left': '700px',
+    'margin-right': '700px',
+    'text-align': 'center',
+    'margin-top': '-50px'
+});
+
+$('.texte').css({
+    'padding': '20px',
+    'text-align': 'center',
+    'color': 'white'
+});
+
+$('.aut').css({
+    'text-align': 'center',
+    'margin-left': '-840px',
+    'color': '#da491c'
+});
+
+$('.box').css({
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-items': 'center'
+});
+
+$('.im').css({
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-items': 'center',
+    'padding': '90px',
+    'width': '300px',
+    'height': '200px',
+    'margin': 'auto',
+    'text-align': 'center'
+});
+
+$('.let').css({
+    'margin-left': '250px',
+    'font-size': '20px'
+});
+
+$('.case').css({
+    'margin-left': '222px',
+    'padding': '25px'
+});
+
+$('.butt').css({
+    'color': 'white',
+    'background-color': '#da491c',
+    'border': '10px',
+    'border-radius': '6px',
+    'padding': '10px',
+    'margin-left': '250px'
+});
